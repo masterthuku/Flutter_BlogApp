24f9b99415281a64c8af36aab7dd4a313d647b22
masterthuku <peterthuku706@gmail.com>
2025-06-28 22:48:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:blog/core/theme/theme.dart';
+import 'package:blog/features/auth/presantation/pages/signup_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blog App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkThemeMode,
+      home: const SignUpPage(),
+    );
+  }
+}
